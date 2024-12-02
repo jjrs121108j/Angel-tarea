739409f93221d56437ebc805d2046364c4801dc7
jjrs121108j <robledojose9736@gmail.com>
2024-12-02 00:39:03 -0600

    Add files via upload

---
+++ java.js
@@ -0,0 +1,4 @@
+// Inicializa AOS (Animate On Scroll) cuando el DOM está completamente cargado
+document.addEventListener('DOMContentLoaded', function () {
+    AOS.init(); // Inicializa las animaciones de AOS
+});
